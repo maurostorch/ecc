e01c03cdc1e8a3d87f3d03cdefe4ea5ce1eb7ea8
Israel Oliveira <israelco@gmail.com>
2013-11-14 16:14:04 -0200

    Encrypt/Decrypt working

--- Ecc.py
+++ Ecc.py
@@ -3,8 +3,12 @@ from os import urandom
 from fractions import gcd
 from gmpy2 import invert
 import sys
+import hashlib 
 from multiprocessing import Pool
+from Crypto.Cipher import AES
+from Crypto import Random
 
+#Generate a radom prime number 
 def prime(size):
 	r = random.Random()
 	while True:
@@ -28,6 +32,7 @@ def PointList(x,y,a,mod):
 def PointMultiplication(x,y,a,mod,i):
 	#print str(i)+"P\n"
 	p=(x,y)
+	pnew = None
 	try:
 		for bpos in range(len(bin(i))-3):
 			#print "-- bpos = " + str(bpos)
@@ -70,11 +75,14 @@ def PointDoubling(x1,y1,a,mod):
 	except:
 		raise	
 
+
+#Verify if a point is on curve
 def isPointOnCurve(x,y,a,b,mod):
 	pc = ((pow(x,3) + (a * x) + b)) % mod
 	p  = pow(y,2) 
 	return p == pc
 
+#Generate a Eliptic Curve
 def CurveGenerate(bitsize):
 	while True:
 		p=prime(bitsize)
@@ -89,6 +97,8 @@ def CurveGenerate(bitsize):
 			break
 	return p,a,b,pts
 
+
+#Generate a KeyPair
 def KeyPairGen(x,y,a,p):
 	while True:
 		try:
@@ -98,15 +108,48 @@ def KeyPairGen(x,y,a,p):
 		except:
 			pass
 
+
+#Encrypt using AES
+def Encrypt(pk,mod,plaintext):
+	#generate a radom integer number
+	r = random.randint(1,mod)
+	#generate R returns aside the ciphertext
+	R = PointMultiplication(5,1,2,mod,r)
+	#generate S - Point which x is the syncronous key for AES
+	S = PointMultiplication(pk[0],pk[1],2,mod,r)
+	#generate K - K is a hash SHA256 of S.x
+	K = hashlib.sha256(str(S[0])).digest()
+	BLOCK_SIZE = 16
+	iv = Random.get_random_bytes(BLOCK_SIZE)
+	Obj = AES.new(K, AES.MODE_CBC,iv)
+	#MODE_C = obs.Encrypt(plaintext)
+	fill =  (BLOCK_SIZE - (len(plaintext) % BLOCK_SIZE)) * chr(BLOCK_SIZE - (len(plaintext) % BLOCK_SIZE))
+	C = iv + Obj.encrypt(plaintext + fill) 
+	return(R,C)
+
+
+#Decrypt Using AES
+def Decrypt(R,ciphertext,d,mod):
+	#generate a S
+	S = PointMultiplication(R[0],R[1],2,mod,d)
+	K = hashlib.sha256(str(S[0])).digest()
+	BLOCK_SIZE = 16
+	iv = ciphertext[:16]
+	ciphertext = ciphertext[16:]
+	fill = (BLOCK_SIZE - len(ciphertext) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(ciphertext) % BLOCK_SIZE)
+	Obj = AES.new(K, AES.MODE_CBC,iv)
+	return Obj.decrypt(ciphertext + fill)[:ciphertext.__len__()-2]
+
+
+
 if __name__ == "__main__":
 	print "Teste da Curva Eliptica"
-	#isPointOnCurve(5,1,2,2,17)
-	#p,a,b,pts = CurveGenerate(160)
-	#print p,a,b,pts
-	#print str(KeyPairGen(5,1,2,17))
-	#print p,a,b,pts
-	#1291 978 178 [(51, 26), (53, 28)]
-	#pl = PointList(51,26,978,1291)
-	#print str(pl)
-	#print len(pl)
-	R = PointMultiplication(5,1,2,17,8)
+	p = prime(160)
+	keypair = KeyPairGen(5,1,2,p)
+	print "Envia PK"
+	print keypair[1]
+	print " ------- ENCRYPT -------"
+	R,C = Encrypt(keypair[1],p,"TESTE DE CRIPROGRADIA UTILIZANDO CURVAS EPLIPTICAS")
+	print R
+	print Decrypt(R,C,keypair[0],p)
+	
